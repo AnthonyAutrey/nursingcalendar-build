36cea36b3adef967ed598bc471c8276fad56a43a
Tony Autrey <autrey.anthony@gmail.com>
2018-04-04 20:26:50 -0500

    Added related CRNs to groups in /groups get API.

--- src/api/routes/Groups.php
+++ src/api/routes/Groups.php
@@ -8,11 +8,33 @@ use \Psr\Http\Message\ResponseInterface as Response;
 // Read //
 $app->get('/groups', function (Request $request, Response $response, array $args) {
 	$queryData = getSelectQueryData($request);
-	$queryString = DBUtil::buildSelectQuery('groups', $queryData['fields'], $queryData['where']);
-	$groups = DBUtil::runQuery($queryString);
-	$response->getBody()->write($groups);
+	$queryString = DBUtil::buildSelectQuery('groups natural left outer join groupcrns', $queryData['fields'], $queryData['where']);
+	$joinedGroups = json_decode(DBUtil::runQuery($queryString));
+
+	$groupMap = [];
+	foreach ($joinedGroups as $key => $joinedGroup) {
+		if (!isset($groupMap[$joinedGroup->GroupName])) {
+			if(is_null($joinedGroup->CRN))
+				$crns = [];
+			else
+				$crns = [$joinedGroup->CRN];
+
+			$groupMap[$joinedGroup->GroupName] = [
+				'GroupName' => $joinedGroup->GroupName,
+				'Description' => $joinedGroup->Description,
+				'CRNs' => $crns
+			];
+		} else {
+			array_push($groupMap[$joinedGroup->GroupName]['CRNs'], $joinedGroup->CRN);
+		}
+	}
+	$groups = [];
+	foreach ($groupMap as $key => $value) {
+		array_push($groups, $value);
+	}
+
+	$response->getBody()->write(json_encode($groups));
 	$response = $response->withHeader('Content-type', 'application/json');
 	return $response;
 
-	// Get CRNS!
 })->add($requireAnyRole);
