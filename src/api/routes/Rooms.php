4cf4b93e2e70493e241b92dfd5730ea4a6088983
Tony Autrey <autrey.anthony@gmail.com>
2018-04-04 20:30:02 -0500

    Added create, update, and delete endpoints for /rooms in API.

--- src/api/routes/Rooms.php
+++ src/api/routes/Rooms.php
@@ -5,12 +5,66 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 // Room Routes /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Create //
+$app->put('/rooms', function (Request $request, Response $response, array $args) {
+	$queryDataArray = getInsertQueryData($request);
+	$results = [];
+
+	if (array_key_exists("insertValues", $queryDataArray))
+		$queryDataArray = [$queryDataArray];
+
+	foreach ($queryDataArray as $queryData) {
+		$queryData = getInsertQueryData($request);
+		$queryString = DBUtil::buildInsertQuery('rooms', $queryData['insertValues']);
+		$results[$queryData['insertValues']] = DBUtil::runCommand($queryString);
+	}
+	$response->getBody()->write($results);
+	$response = $response->withHeader('Content-type', 'application/json');
+	return $response;
+})->add($requireAdmin);
+
 // Read //
 $app->get('/rooms', function (Request $request, Response $response, array $args) {
 	$queryData = getSelectQueryData($request);
 	$queryString = DBUtil::buildSelectQuery('rooms natural left outer join roomResourceRelation', $queryData['fields'], $queryData['where']);
 	$rooms = DBUtil::runQuery($queryString);
 	$response->getBody()->write($rooms);
+	$response = $response->withHeader('Content-type', 'application/json');
+	return $response;	
+})->add($requireAnyRole);
+
+// Update //
+$app->post('/rooms', function (Request $request, Response $response, array $args) {
+	$results = [];
+	$queryDataArray = getUpdateQueryData($request);
+
+	if (array_key_exists("setValues",$queryDataArray) && array_key_exists("where",$queryDataArray))
+		$queryDataArray = [$queryDataArray];
+
+	foreach ($queryDataArray as $queryData) {
+		// return with 'bad request' response if request isn't correct
+		if (!isset($queryData['setValues']) ||
+			!count($queryData['setValues']) > 0 ||
+			!isset($queryData['where'])
+			) {
+			return $response->withStatus(400);
+		}
+	
+		$queryString = DBUtil::buildUpdateQuery('rooms', $queryData['setValues'], $queryData['where']);	
+		array_push($results, DBUtil::runCommand($queryString));
+	}
+
+	$response->getBody()->write(json_encode($results));
+	$response = $response->withHeader('Content-type', 'application/json');
+	return $response;
+})->add($requireAdmin);
+
+// Delete //
+$app->delete('/rooms', function (Request $request, Response $response, array $args) {
+	$queryData = getDeleteQueryData($request);
+	$deleteLocationsQuery = DBUtil::buildDeleteQuery('rooms', $queryData['where']);
+	$results = DBUtil::runCommand($deleteLocationsQuery);
+	$response->getBody()->write(json_encode($results));
 	$response = $response->withHeader('Content-type', 'application/json');
 	return $response;	
 })->add($requireAnyRole);
