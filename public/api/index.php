3343598a0aa64dab6f27b2b8a94a02f0e26831e2
Tony Autrey <autrey.anthony@gmail.com>
2018-04-18 19:05:19 -0500

    Another debugging change for deployment

--- public/api/index.php
+++ public/api/index.php
@@ -1,84 +1,86 @@
 <?php
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+echo "Hmm?";
 
-require '../../src/api/dependencies/autoload.php';
-require '../../src/api/util/DBUtil.php';
-require '../../src/api/util/ComputingCenter.php';
-require '../../src/api/util/APIUtil.php';
-require '../../src/api/middleware/Authenticator.php';
+// use \Psr\Http\Message\ServerRequestInterface as Request;
+// use \Psr\Http\Message\ResponseInterface as Response;
 
-// Configuration //////////////////////////////////////////////////////////////////////////////////////////////////////////
+// require '../../src/api/dependencies/autoload.php';
+// require '../../src/api/util/DBUtil.php';
+// require '../../src/api/util/ComputingCenter.php';
+// require '../../src/api/util/APIUtil.php';
+// require '../../src/api/middleware/Authenticator.php';
 
-$config = [
-    'settings' => [
-		'displayErrorDetails' => true,
-	],
-	'devEnvironment' => true
-];
+// // Configuration //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-$app = new \Slim\App($config);
+// $config = [
+//     'settings' => [
+// 		'displayErrorDetails' => true,
+// 	],
+// 	'devEnvironment' => true
+// ];
 
-// Start Session /////////////////////////////////////////////////////////////////////////////////////////////////////////
+// $app = new \Slim\App($config);
 
-$app->add(function ($request, $response, $next) {
-	session_start();
-	return $next($request, $response);	
-});
+// // Start Session /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Authentication Middleware /////////////////////////////////////////////////////////////////////////////////////////////
+// $app->add(function ($request, $response, $next) {
+// 	session_start();
+// 	return $next($request, $response);	
+// });
 
-$requireAnyRole = new Authenticator(['student', 'instructor', 'administrator']);
-$requireInstructorOrAdmin = new Authenticator(['instructor', 'administrator']);
-$requireAdmin = new Authenticator(['administrator']);
+// // Authentication Middleware /////////////////////////////////////////////////////////////////////////////////////////////
 
-// Import Routes ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// $requireAnyRole = new Authenticator(['student', 'instructor', 'administrator']);
+// $requireInstructorOrAdmin = new Authenticator(['instructor', 'administrator']);
+// $requireAdmin = new Authenticator(['administrator']);
 
-require '../../src/api/routes/Authentication.php';
-require '../../src/api/routes/Events.php';
-require '../../src/api/routes/Rooms.php';
-require '../../src/api/routes/Locations.php';
-require '../../src/api/routes/Resources.php';
-require '../../src/api/routes/Users.php';
-require '../../src/api/routes/UserGroups.php';
-require '../../src/api/routes/Preferences.php';
-require '../../src/api/routes/Groups.php';
-require '../../src/api/routes/Logs.php';
-require '../../src/api/routes/Notifications.php';
-require '../../src/api/routes/OverrideRequests.php';
-require '../../src/api/routes/PublishDates.php';
+// // Import Routes ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Error Handling /////////////////////////////////////////////////////////////////////////////////////////////////////
+// require '../../src/api/routes/Authentication.php';
+// require '../../src/api/routes/Events.php';
+// require '../../src/api/routes/Rooms.php';
+// require '../../src/api/routes/Locations.php';
+// require '../../src/api/routes/Resources.php';
+// require '../../src/api/routes/Users.php';
+// require '../../src/api/routes/UserGroups.php';
+// require '../../src/api/routes/Preferences.php';
+// require '../../src/api/routes/Groups.php';
+// require '../../src/api/routes/Logs.php';
+// require '../../src/api/routes/Notifications.php';
+// require '../../src/api/routes/OverrideRequests.php';
+// require '../../src/api/routes/PublishDates.php';
 
-if (!$config['devEnvironment']) {
+// // Error Handling /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	$container = $app->getContainer();
+// if (!$config['devEnvironment']) {
 
-	$container['errorHandler'] = function ($container) {
-		return function ($request, $response, $exception) use ($container) {
-			// TODO: retrieve logger from $container here and log the error
-			$response->getBody()->rewind();
-			return $response->withStatus(500)
-							->withHeader('Content-Type', 'text/html')
-							->write("Oops, something's gone wrong!");
-		};
-	};
+// 	$container = $app->getContainer();
 
-	$container['phpErrorHandler'] = function ($container) {
-		return function ($request, $response, $error) use ($container) {
-			// TODO: retrieve logger from $container here and log the error
-			$response->getBody()->rewind();
-			return $response->withStatus(500)
-							->withHeader('Content-Type', 'text/html')
-							->write("Oops, something's gone wrong!");
-		};
-	};
+// 	$container['errorHandler'] = function ($container) {
+// 		return function ($request, $response, $exception) use ($container) {
+// 			// TODO: retrieve logger from $container here and log the error
+// 			$response->getBody()->rewind();
+// 			return $response->withStatus(500)
+// 							->withHeader('Content-Type', 'text/html')
+// 							->write("Oops, something's gone wrong!");
+// 		};
+// 	};
 
-	error_reporting(E_ALL & ~E_NOTICE);
+// 	$container['phpErrorHandler'] = function ($container) {
+// 		return function ($request, $response, $error) use ($container) {
+// 			// TODO: retrieve logger from $container here and log the error
+// 			$response->getBody()->rewind();
+// 			return $response->withStatus(500)
+// 							->withHeader('Content-Type', 'text/html')
+// 							->write("Oops, something's gone wrong!");
+// 		};
+// 	};
 
-}
+// 	error_reporting(E_ALL & ~E_NOTICE);
 
-// Execute /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// }
 
-$app->run();
+// // Execute /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// $app->run();
